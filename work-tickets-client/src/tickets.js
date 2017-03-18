0abe65474026893880af0a50d35d225541b445e1
Marko Svaljek <msvaljek@gmail.com>
2017-03-18 15:04:57 +0100

    WT-0 listing tickets and back button functionality

--- work-tickets-client/src/tickets.js
+++ work-tickets-client/src/tickets.js
@@ -8,13 +8,18 @@ export class Home {
 
     constructor(http, router) {
         this.http = http;
-        this.projects = [];
+        this.tickets = [];
         this.year = new Date().getFullYear();
+        this.router = router
     }
 
     activate() {
-        return this.http.fetch('http://localhost:8080/projects')
+        return this.http.fetch('http://localhost:8080/projects/WT/tickets')
             .then(response => response.json())
-            .then(data => this.projects = data)
+            .then(data => this.tickets = data)
+    }
+
+    goToHome() {
+        this.router.navigateToRoute('home')
     }
 }
